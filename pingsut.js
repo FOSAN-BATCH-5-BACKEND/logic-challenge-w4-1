d15c9c54a17bfa15eb5efda64830ab1036a4dc57
Rijal Fadhila <r3dlabelz@gmail.com>
2021-05-10 12:51:40 +0700

    dikerjakan sambil minum es kelapa muda

---
+++ pingsut.js
@@ -0,0 +1,19 @@
+/* 
+Rama dan Rian sedang bermain di halaman depan Fosan mempeributkan cilok yang tinggal sebungkus,
+akhirnya mereka memutuskan untuk membuat function pingsut, yang didalamnya ada 2 parameter yaitu Rama dan Rian,
+buatlah fungsi untuk menentukan siapa yang menang dalam pingsut tersebut
+*/
+
+
+function pingsut(Rama, Rian) {
+	//buat kodemu disini
+}
+
+
+
+console.log(pingsut("Rock", "Paper"))  		// "Pemenangnya Rian"
+console.log(pingsut("Scissors", "Rock")) 	// "Pemenangnya Rian"
+console.log(pingsut("Scissors", "Paper")) 	// "Pemenangnya Rama"
+console.log(pingsut("Paper", "Rock")) 		// "Pemenangnya Rama"
+console.log(pingsut("Paper", "Paper")) 		// "Hasilnya draw"
+console.log(pingsut("Rock", "Rock"))  		// "Hasilnya draw"
---
+++ tabelPerkalian.js
@@ -0,0 +1,64 @@
+/* 
+INSTRUKSI
+=========
+buatlah tabel perkalian 1-10 berdasarkan angka yang di minta
+misalkan yang di minta adalah 5, maka jawabannya:
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+
+*/
+
+
+function tabelPerkalian(angka) {
+   
+  }
+
+console.log(tabelPerkalian(5))
+/* 
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+*/
+
+console.log(tabelPerkalian(7))
+/*
+1 * 7 = 7
+2 * 7 = 14
+3 * 7 = 21
+4 * 7 = 28
+5 * 7 = 35
+6 * 7 = 42
+7 * 7 = 49
+8 * 7 = 56
+9 * 7 = 63
+10 * 7 = 70
+*/
+
+console.log(tabelPerkalian(13))
+/*
+1 * 13 = 13
+2 * 13 = 26
+3 * 13 = 39
+4 * 13 = 52
+5 * 13 = 65
+6 * 13 = 78
+7 * 13 = 91
+8 * 13 = 104
+9 * 13 = 117
+10 * 13 = 130
+*/
